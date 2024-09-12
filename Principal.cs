2f230dc0345edea666e637c831b5e76f3838907c
marialeticia <galdino.leticia@escolar.ifrn.edu.br>
2024-09-12 16:34:51 -0300

    frist commit

---
+++ Principal.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Principal
+{
+    static void Main(string[] agrs)
+    {
+        Conta minhaConta = new Conta("Joao dos santos", 12378, 4537, 437345645);
+
+        Conta outraConta = new Conta("Paulo Bezerra", 0532, 4321, 257801389);
+    } 
+}
---
+++ conta.cs
@@ -0,0 +1,18 @@
+﻿public class Conta  
+
+{
+    public string nome;
+    public int numero;
+    public int agencia;
+    public int cpf;
+
+
+ public Conta(string nomeDoProprietario, int numeroDaConta, int agenciaDoBanco, int numeroDoCpf)
+ {
+     nome = nomeDoProprietario;
+     numero = numeroDaConta;
+     agencia = agenciaDoBanco;
+     cpf = numeroDoCpf;
+ }
+
+}
